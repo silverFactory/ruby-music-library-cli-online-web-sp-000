661bf66b34568af558f0ddb7d12faaf24a1ee2c1
Will Studabaker <will.studabaker@gmail.com>
2020-11-06 17:37:21 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -14,5 +14,6 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
+    input = gets.chomp
   end
 end
